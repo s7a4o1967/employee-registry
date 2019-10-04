beb17250e30caa5179523ae464fe5e7a3a47585a
Chandraprakash Kistaiahgari <chandraprakash.n.kistaiahgari@gsk.com>
2019-10-04 19:24:44 +0530

    updated repository with annotation

--- employee-registry/src/main/java/com/example/repository/LoginRepository.java
+++ employee-registry/src/main/java/com/example/repository/LoginRepository.java
@@ -2,8 +2,9 @@ package com.example.repository;
 
 import com.example.entity.LoginEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LoginRepository extends JpaRepository<LoginEntity,String>{
-
     LoginEntity findByEmpIdAndPassword(String empId,String password);
 }
